0b362885d1f1925437330d6ec14c4f6761c26ef3
Fábio Maia <fabio.maia@itsector.pt>
2022-01-26 19:20:47 +0000

    Refined code and storage structs to optimize insert and calculate data

--- src/token/contracts/longevity.sol
+++ src/token/contracts/longevity.sol
@@ -9,17 +9,24 @@ contract Longevity is ERC721 {
     string private nextContestId;
     DateTime private dateTimeUtils;
     uint256 private available;
-    uint256 private count;
 
     uint256 private constant SUPPLY = 54000000000;
     uint256 private constant ONE_DAY = 24 * 60 * 60;
+    uint256 private MINIMAL_INNAPROPRIATE_VOTES = 5;
+    uint256 private MINIMAL_PERCENTAGE_INNAPROPRIATE_VOTES = 50;
 
     event Logger(string msg);
 
-    struct ImageUpload {
+    struct Votation {
+        bool like;
+        bool innapropriate;
+    }
+
+    struct Image {
         address wallet;
         uint256 timestamp;
-        uint256 votes;
+        mapping(string => Votation) votation;
+        uint256 likeVotes;
         uint256 innapropriateVotes;
         bool active;
     }
@@ -29,11 +36,17 @@ contract Longevity is ERC721 {
         address wallet;
     }
 
-    mapping(string => mapping(string => ImageUpload)) private contests;
+    string[] private currentContestImages;
+    uint256 private currentContestImagesLength;
+    string[] private nextContestImages;
+    uint256 private nextContestImagesLength;
+    mapping(string => mapping(string => Image)) private contests;
     mapping(string => Winner) private winners;
 
     constructor() ERC721("Longevity", "LGT") {
-        count = 0;
+        currentContestImagesLength = 0;
+        nextContestImagesLength = 0;
+
         available = SUPPLY;
         dateTimeUtils = new DateTime();
 
@@ -51,7 +64,7 @@ contract Longevity is ERC721 {
     }
     */
 
-    // modifier restrictedPast24h(ImageUpload memory image) {
+    // modifier restrictedPast24h(Image memory image) {
     //     require(
     //         image.timestamp + ONE_DAY < block.timestamp,
     //         "This function is restricted to not doing changes past 24 hours"
@@ -59,7 +72,7 @@ contract Longevity is ERC721 {
     //     _;
     // }
 
-    /* Getters */
+    /** GETTERS */
     function getCurrentContestId() public view returns (string memory) {
         return currentContestId;
     }
@@ -68,32 +81,22 @@ contract Longevity is ERC721 {
         return nextContestId;
     }
 
-    function getImageData(string memory imageId)
+    function getImageLikeVotes(string memory imageId)
         public
         view
-        returns (ImageUpload memory)
+        returns (uint256)
     {
-        ImageUpload memory imageForCurrentContest = contests[currentContestId][
-            imageId
-        ];
-        ImageUpload memory imageForNextContest = contests[nextContestId][
-            imageId
-        ];
-
-        require(
-            imageForCurrentContest.active || imageForNextContest.active,
-            "Image doesn't exists"
-        );
-
-        if (imageForCurrentContest.active) {
-            return imageForCurrentContest;
-        } else {
-            return imageForNextContest;
+        if (contests[currentContestId][imageId].active) {
+            return contests[currentContestId][imageId].likeVotes;
+        } else if (contests[nextContestId][imageId].active) {
+            return contests[nextContestId][imageId].likeVotes;
         }
+
+        return 0;
     }
 
     function getVersion() public view returns (string memory) {
-        return "0.2.0";
+        return "0.3.0";
     }
 
     function getWinnerByDate(string memory date)
@@ -104,7 +107,7 @@ contract Longevity is ERC721 {
         return winners[date];
     }
 
-    /* Utitilities */
+    /** UTILITIES */
     function convertStringDate2UIntTime(string memory date)
         private
         view
@@ -119,8 +122,7 @@ contract Longevity is ERC721 {
         return dateTimeUtils.getDate(timestamp);
     }
 
-    /* Logical functions */
-
+    /* PRIVATE METHODS - BUSINESS LOGIC */
     function startDailyContests() private {
         currentContestId = dateTimeUtils.getDate(block.timestamp);
         nextContestId = dateTimeUtils.getDate(block.timestamp, 1);
@@ -128,102 +130,131 @@ contract Longevity is ERC721 {
         emit Logger("startDailyContests");
     }
 
-    function calculateWinnerDailyContest() private {
-        emit Logger("calculateWinnerDailyContest");
-    }
-
-    function transfer(address wallet, uint256 qtd) private {
-        payable(wallet).transfer(qtd);
-    }
-
-    function contestsLength() public view returns (uint256) {
-        return count;
-    }
-
+    /** PUBLIC METHODS - API */
     function UploadImage(string memory imageId) public returns (bool) {
         emit Logger("UploadImage > start");
-        ImageUpload memory imageUpload = ImageUpload({
+
+        Votation memory votation = Votation({
+            like: false,
+            innapropriate: false
+        });
+
+        Image memory image = Image({
             wallet: msg.sender,
             timestamp: block.timestamp,
-            votes: 0,
+            votation: votation,
+            likeVotes: 0,
             innapropriateVotes: 0,
             active: true
         });
 
-        contests[nextContestId][imageId] = imageUpload;
-        count++;
+        nextContestImages.push(image);
+        nextContestImagesLength++;
+        contests[nextContestId][imageId] = image;
 
         emit Logger("UploadImage > end");
 
         return true;
     }
 
-    function Vote(string memory imageId) public {
-        emit Logger("Vote > start");
+    function LikeVote(string memory imageId) public {
+        emit Logger("LikeVote > start");
 
-        ImageUpload memory imageUploadForCurrentContest = contests[
-            currentContestId
-        ][imageId];
-        ImageUpload memory imageUploadForNextContest = contests[nextContestId][
+        Image memory imageForCurrentContest = contests[currentContestId][
             imageId
         ];
+        Image memory imageForNextContest = contests[nextContestId][imageId];
 
         if (
-            imageUploadForCurrentContest.active &&
-            imageUploadForCurrentContest.timestamp + ONE_DAY >= block.timestamp
+            imageForCurrentContest.active &&
+            imageForCurrentContest.timestamp + ONE_DAY >= block.timestamp
         ) {
-            emit Logger("Vote > current contest");
+            emit Logger("LikeVote > current contest");
 
-            imageUploadForCurrentContest.votes++;
-            contests[currentContestId][imageId] = imageUploadForCurrentContest;
+            imageForCurrentContest.votes++;
+            contests[currentContestId][imageId] = imageForCurrentContest;
         }
 
         if (
-            imageUploadForNextContest.active &&
-            imageUploadForNextContest.timestamp + ONE_DAY >= block.timestamp
+            imageForNextContest.active &&
+            imageForNextContest.timestamp + ONE_DAY >= block.timestamp
         ) {
-            emit Logger("Vote > next contest");
+            emit Logger("LikeVote > next contest");
 
-            imageUploadForNextContest.votes++;
+            imageForNextContest.votes++;
 
-            contests[nextContestId][imageId] = imageUploadForNextContest;
+            contests[nextContestId][imageId] = imageForNextContest;
         }
 
-        emit Logger("Vote > end");
+        emit Logger("LikeVote > end");
     }
 
     function InnapropriateVote(string memory imageId) public {
         emit Logger("InnapropriateVote > start");
 
-        ImageUpload memory imageUploadForCurrentContest = contests[
-            currentContestId
-        ][imageId];
-        ImageUpload memory imageUploadForNextContest = contests[nextContestId][
+        Image memory imageForCurrentContest = contests[currentContestId][
             imageId
         ];
+        Image memory imageForNextContest = contests[nextContestId][imageId];
 
         if (
-            imageUploadForCurrentContest.active &&
-            imageUploadForCurrentContest.timestamp + ONE_DAY >= block.timestamp
+            imageForCurrentContest.active &&
+            imageForCurrentContest.timestamp + ONE_DAY >= block.timestamp
         ) {
             emit Logger("InnapropriateVote > current contest");
 
-            imageUploadForCurrentContest.innapropriateVotes++;
+            imageForCurrentContest.innapropriateVotes++;
 
-            contests[currentContestId][imageId] = imageUploadForCurrentContest;
+            contests[currentContestId][imageId] = imageForCurrentContest;
         }
 
         if (
-            imageUploadForNextContest.active &&
-            imageUploadForNextContest.timestamp + ONE_DAY >= block.timestamp
+            imageForNextContest.active &&
+            imageForNextContest.timestamp + ONE_DAY >= block.timestamp
         ) {
             emit Logger("InnapropriateVote > next contest");
 
-            imageUploadForNextContest.innapropriateVotes++;
+            imageForNextContest.innapropriateVotes++;
 
-            contests[nextContestId][imageId] = imageUploadForNextContest;
+            contests[nextContestId][imageId] = imageForNextContest;
         }
 
         emit Logger("InnapropriateVote > end");
+    }
+
+    function CalculateWinnerDailyContest() public {
+        emit Logger("CalculateWinnerDailyContest");
+
+        startDailyContests();
+
+        currentContestImages = nextContestImages;
+        currentContestImagesLength = nextContestImagesLength;
+        nextContestImages = [];
+        nextContestImagesLength = 0;
+
+        mapping(string => Image) memory currentContest = contests[
+            currentContestId
+        ];
+        Image memory mostLikedImage = currentContest[currentContestImages[0]];
+
+        for (uint256 i = 1; i < currentContestImages.lenght; i++) {
+            if (
+                mostLikedImage.votation.like <
+                currentContest[currentContestImages[i]].votation.like &&
+                (currentContest[currentContestImages[i]]
+                    .votation
+                    .innapropriate * 100) /
+                    currentContest[currentContestImages[i]].votation.like >=
+                MINIMAL_PERCENTAGE_INNAPROPRIATE_VOTES &&
+                currentContest[currentContestImages[i]]
+                    .votation
+                    .innapropriate >=
+                MINIMAL_INNAPROPRIATE_VOTES
+            ) {}
+        }
+    }
+
+    function transfer(address wallet, uint256 qtd) public {
+        payable(wallet).transfer(qtd);
     }
 }
